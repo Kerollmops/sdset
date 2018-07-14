ffc7a4fda2b7160a79b4a396468c23d2fc54b695
Kerollmops <renault.cle@gmail.com>
2018-07-14 17:40:11 +0200

    feat: Make the union for two slices an Iterator

--- src/lib.rs
+++ src/lib.rs
@@ -5,48 +5,63 @@ extern crate test;
 // FIXME allow to use #![no_std]
 use std::cmp::Ordering;
 
+pub struct UnionTwoSlices<'a, T: 'a> {
+    a: &'a [T],
+    b: &'a [T],
+}
+
+impl<'a, T: 'a> UnionTwoSlices<'a, T> {
+    pub fn new(a: &'a [T], b: &'a [T]) -> Self {
+        UnionTwoSlices { a, b }
+    }
+}
+
 #[inline]
 fn skip_duplicates<'a, T: Eq>(slice: &'a [T], elem: &'a T) -> &'a [T] {
     let count = slice.iter().take_while(|x| *x == elem).count();
     &slice[count..]
 }
 
-pub fn union_two_slices<T>(mut a: &[T], mut b: &[T]) -> Vec<T>
-where T: Ord + Clone
-{
-    let mut output = Vec::new();
+impl<'a, T: 'a + Ord> Iterator for UnionTwoSlices<'a, T> {
+    type Item = &'a T;
 
-    while !a.is_empty() && !b.is_empty() {
-        match a[0].cmp(&b[0]) {
-            Ordering::Less => {
-                output.push(a[0].clone());
-                a = skip_duplicates(a, &a[0]);
-            },
-            Ordering::Equal => {
-                output.push(a[0].clone());
-                a = skip_duplicates(a, &a[0]);
-                b = skip_duplicates(b, &b[0]);
-            },
-            Ordering::Greater => {
-                output.push(b[0].clone());
-                b = skip_duplicates(b, &b[0]);
-            },
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.a.is_empty() && !self.b.is_empty() {
+            match self.a[0].cmp(&self.b[0]) {
+                Ordering::Less => {
+                    let ret = &self.a[0];
+                    self.a = skip_duplicates(self.a, &self.a[0]);
+                    return Some(ret);
+                },
+                Ordering::Equal => {
+                    let ret = &self.a[0];
+                    self.a = skip_duplicates(self.a, &self.a[0]);
+                    self.b = skip_duplicates(self.b, &self.b[0]);
+                    return Some(ret);
+                },
+                Ordering::Greater => {
+                    let ret = &self.b[0];
+                    self.b = skip_duplicates(self.b, &self.b[0]);
+                    return Some(ret);
+                },
+            }
         }
-    }
 
-    // if b is empty before a, add all remaining elements of a
-    while !a.is_empty() {
-        output.push(a[0].clone());
-        a = skip_duplicates(a, &a[0]);
-    }
+        if !self.a.is_empty() {
+            let ret = &self.a[0];
+            self.a = skip_duplicates(self.a, &self.a[0]);
+            Some(ret)
+        }
+        else if !self.b.is_empty() {
+            let ret = &self.b[0];
+            self.b = skip_duplicates(self.b, &self.b[0]);
+            Some(ret)
+        }
+        else {
+            None
+        }
 
-    // idem but if a is empty before b
-    while !b.is_empty() {
-        output.push(b[0].clone());
-        b = skip_duplicates(b, &b[0]);
     }
-
-    output
 }
 
 #[cfg(test)]
@@ -59,9 +74,9 @@ mod tests {
         let a = &[1, 2, 3];
         let b = &[2, 3, 4];
 
-        let union_ = union_two_slices(a, b);
+        let union: Vec<_> = UnionTwoSlices::new(a, b).cloned().collect();
 
-        assert_eq!(&union_, &[1, 2, 3, 4]);
+        assert_eq!(&union, &[1, 2, 3, 4]);
     }
 
     #[test]
@@ -69,9 +84,9 @@ mod tests {
         let a = &[1, 2, 2, 3, 3];
         let b = &[2, 3, 3, 4];
 
-        let union_ = union_two_slices(a, b);
+        let union: Vec<_> = UnionTwoSlices::new(a, b).cloned().collect();
 
-        assert_eq!(&union_, &[1, 2, 3, 4]);
+        assert_eq!(&union, &[1, 2, 3, 4]);
     }
 
     #[test]
@@ -79,9 +94,9 @@ mod tests {
         let a = &[1, 2, 3, 4];
         let b = &[2, 3, 4];
 
-        let union_ = union_two_slices(a, b);
+        let union: Vec<_> = UnionTwoSlices::new(a, b).cloned().collect();
 
-        assert_eq!(&union_, &[1, 2, 3, 4]);
+        assert_eq!(&union, &[1, 2, 3, 4]);
     }
 
     #[test]
@@ -89,9 +104,9 @@ mod tests {
         let a = &[1, 2, 2, 3, 3];
         let b = &[];
 
-        let union_ = union_two_slices(a, b);
+        let union: Vec<_> = UnionTwoSlices::new(a, b).cloned().collect();
 
-        assert_eq!(&union_, &[1, 2, 3]);
+        assert_eq!(&union, &[1, 2, 3]);
     }
 
     #[test]
@@ -99,9 +114,9 @@ mod tests {
         let a = &[];
         let b = &[2, 3, 3, 4];
 
-        let union_ = union_two_slices(a, b);
+        let union: Vec<_> = UnionTwoSlices::new(a, b).cloned().collect();
 
-        assert_eq!(&union_, &[2, 3, 4]);
+        assert_eq!(&union, &[2, 3, 4]);
     }
 
     #[test]
@@ -109,9 +124,9 @@ mod tests {
         let a = &[1, 1, 1, 1];
         let b = &[1, 1, 1, 1, 1];
 
-        let union_ = union_two_slices(a, b);
+        let union: Vec<_> = UnionTwoSlices::new(a, b).cloned().collect();
 
-        assert_eq!(&union_, &[1]);
+        assert_eq!(&union, &[1]);
     }
 
     #[bench]
@@ -120,8 +135,8 @@ mod tests {
             let a = &[1, 2, 3];
             let b = &[2, 3, 4];
 
-            let union_ = union_two_slices(a, b);
-            test::black_box(|| union_);
+            let union: Vec<_> = UnionTwoSlices::new(a, b).cloned().collect();
+            test::black_box(|| union);
         });
     }
 
@@ -131,8 +146,8 @@ mod tests {
             let a = &[1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
             let b = &[2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4];
 
-            let union_ = union_two_slices(a, b);
-            test::black_box(|| union_);
+            let union: Vec<_> = UnionTwoSlices::new(a, b).cloned().collect();
+            test::black_box(|| union);
         });
     }
 }
