b553759fa136647987d2c6c3327684ddcc4ceb4c
Kerollmops <renault.cle@gmail.com>
2018-08-15 11:52:55 +0200

    feat: Add a script to compare benchmarks based on commits

---
+++ bench_script.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# This env variable is here to permit
+# to use a custom `cargo bench` command if needed
+CARGO_BENCH_CMD=${CARGO_BENCH_CMD:-cargo bench}
+
+if [ $# -eq 0 ]; then
+    echo "comparing benchmarks of HEAD~1 and HEAD..."
+    OLD=$(git rev-parse --short 'HEAD~1')
+    NEW=$(git rev-parse --short 'HEAD')
+elif [ $# -eq 1 ]; then
+    echo "comparing benchmarks of $1 and HEAD..."
+    OLD=$(git rev-parse --short $1)
+    NEW=$(git rev-parse --short 'HEAD')
+elif [ $# -eq 2 ]; then
+    echo "comparing benchmarks of $1 and $2..."
+    OLD=$(git rev-parse --short $1)
+    NEW=$(git rev-parse --short $2)
+else
+    echo 'Usage: bench_script.sh [$OLD] [$NEW]'
+    exit 1
+fi
+
+if ! git diff-files --quiet; then
+    echo 'Your repository must not be dirty'
+    exit 1
+fi
+
+if [ ! -f $NEW.bench ]; then
+    git checkout $NEW
+    $CARGO_BENCH_CMD > $NEW.bench
+    git checkout -
+fi
+
+if [ ! -f $OLD.bench ]; then
+    git checkout $OLD
+    $CARGO_BENCH_CMD > $OLD.bench
+    git checkout -
+fi
+
+cargo benchcmp --threshold 5 $OLD.bench $NEW.bench
